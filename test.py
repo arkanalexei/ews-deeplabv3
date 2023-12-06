5b673c762e341ca3a845c15bcdcc04e03d6c24f7
arkan.alexei <arkan.alexei@ui.ac.id>
2023-12-06 17:40:10 +0700

    remove unecessary data

--- test.py
+++ test.py
@@ -1,27 +1,8 @@
 import torch
-from methods.proposed import DeepLabV3PlusInjNet
+from methods.proposed import proposed_model
 import segmentation_models_pytorch as smp
 import torch.optim
 import torch.backends.cudnn
-
-
-def proposed_model():
-    network = smp.DeepLabV3Plus('resnet50', classes=2)
-
-    name = 'dlb50_inj35ft15'
-    inputs = ['image', 'fnumber', 'exposure', 'iso']
-    outputs = ['mask']
-    inj_layers = [None, None, None, None, None, ['fnumber', 'exposure', 'iso']]
-    inj_type = 'concat'
-
-    for mod_name, module in network.encoder.named_children():
-        if mod_name in ['layer2', 'layer3']:
-            for param in module.parameters():
-                param.requires_grad = False
-
-    model = DeepLabV3PlusInjNet(name, inputs, outputs, network, inj_layers, inj_type)
-
-    return model
 
 
 model = proposed_model()
