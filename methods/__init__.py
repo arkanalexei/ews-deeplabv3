fb55a3f1b53b819a517021892fe1c24bac223a15
arkan.alexei <arkan.alexei@ui.ac.id>
2023-12-06 17:30:16 +0700

    remove unecessary methods

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,4 +1,2 @@
 import methods.base
-import methods.sadeghi_et_al_2017
-import methods.yu_et_al_2017
 import methods.proposed
--- methods/proposed.py
+++ methods/proposed.py
@@ -25,7 +25,7 @@ class Proposed(base.BenchmarkMethod):
 
         self.model = proposed_model()
 
-        self.epochs = 2 # CHANGE TO 150 FOR THE REAL RUN
+        self.epochs = 5 # CHANGE TO 150 FOR THE REAL RUN
         self.batchsize = 16
         self.eval_batchsize = 4
 
