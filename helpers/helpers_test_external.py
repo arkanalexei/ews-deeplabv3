23258163b24568aff391354ed2c88b3653f16e38
arkan.alexei <arkan.alexei@ui.ac.id>
2023-12-09 22:52:06 +0700

    batch 2 results

---
+++ helpers/helpers_test_external.py
@@ -0,0 +1,77 @@
+import torch
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+from helpers.helper_fcts import EncodedToTensor, Normalize, ScaleImage, PadInput
+
+
+def resize_image(image_pil, target_size=(350, 350)):
+    return image_pil.resize(target_size, Image.ANTIALIAS)
+
+def transform_image_external(image):
+    """
+    Transform image as dictated by the referenced paper.
+
+    Keyword arguments:
+    image -- The image itself.
+    """
+    image_pil = Image.fromarray((image * 255).astype('uint8'))
+    image_pil = resize_image(image_pil)
+    image_dict = {'image': image_pil}
+
+    transforms = []
+    transforms.append(EncodedToTensor())
+    transforms.append(Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
+    transforms.append(ScaleImage(2.0))
+    transforms.append(PadInput((704, 704)))
+
+    for transform in transforms:
+        image_dict = transform(image_dict)
+
+    return image_dict['image'].unsqueeze(0)
+
+
+def default_metadata():
+    """
+    Return default metadata values.
+    """
+    iso = np.float(100)
+    iso = np.log2(iso / 100)
+    fnumber = np.float(8)
+    exposure = np.float(1/90)
+
+    iso = torch.tensor([iso])
+    fnumber = torch.tensor([fnumber])
+    exposure = torch.tensor([exposure])
+
+    return iso, fnumber, exposure
+
+
+def visualize_results_external(image, output, filename):
+    """
+    Visualize segmentation result
+
+    Keyword arguments:
+    image -- Original image
+    output -- Segmentation result
+    filename -- Filename to save the figure
+    """
+    fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+
+    # Original Image
+    axes[0].imshow(image)
+    axes[0].set_title("Original Image")
+    axes[0].axis('off')
+
+    # Segmentation Result
+    segmentation_result = torch.argmax(output['mask'].squeeze(0), dim=0)
+    axes[1].imshow(segmentation_result.cpu(), cmap='gray')
+    axes[1].set_title("Segmentation Result")
+    axes[1].axis('off')
+
+    plt.subplots_adjust(wspace=0.1, hspace=0)
+    fig.tight_layout(pad=1)
+
+    plt.savefig(f'results-external/2/{filename}.png', bbox_inches='tight')
+    plt.close(fig)
---
+++ no_mask_test.py
@@ -0,0 +1,40 @@
+import torch
+import glob
+import os
+
+from methods.proposed import proposed_model
+from methods import base
+from helpers.helpers_test_external import default_metadata, transform_image_external, visualize_results_external
+
+
+# Load the model
+model = proposed_model()
+checkpoint = torch.load('best_model.pth', map_location='cpu')
+model.load_state_dict(checkpoint['model_state_dict'])
+model.eval()
+
+# Save output
+path = 'data/test-external/2/'
+paths = glob.glob(path + '*.png')
+
+for image_path in paths:
+    # Load image
+    image = base.BenchmarkMethod.read_image(image_path, rgb=True)
+    image_transformed = transform_image_external(image)
+
+    # Extract metadata of training image
+    iso, fnumber, exposure = default_metadata()
+
+    # Prediction
+    model_input = {
+        'image': image_transformed.to('cpu'),
+        'iso': iso,
+        'fnumber': fnumber,
+        'exposure': exposure
+    }
+
+    with torch.no_grad():
+        output = model(model_input)
+
+    filename = os.path.basename(image_path).split('.')[0]
+    visualize_results_external(image, output, filename)
